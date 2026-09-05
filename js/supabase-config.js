65457b40a2934370f34646abac21e36101a7481c
RafiHana <krsmaima@gmail.com>
2026-09-06 03:19:50 +0700

    Update data

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL      = 'https://swejahgzcazzpbezfzbd.supabase.co/rest/v1/';
+const SUPABASE_URL      = 'https://swejahgzcazzpbezfzbd.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3ZWphaGd6Y2F6enBiZXpmemJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4OTcwNDQsImV4cCI6MjEwMTQ3MzA0NH0.iUhEg2RCiI3fo8wn9008uXVJ_BAi02deS3sY_CpZ9Sg';
 
 const { createClient } = supabase;
